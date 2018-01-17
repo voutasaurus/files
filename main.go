11bf6ccb025307c837805b7f5fa5b80542d245a0
voutasaurus <voutasaurus@gmail.com>
2018-01-17 15:52:26 -0800

    add fileserver

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	filename := flag.String("f", "", "the file to serve")
+	flag.Parse()
+
+	log.SetFlags(0)
+
+	if _, err := os.Stat(*filename); os.IsNotExist(err) {
+		log.Fatalf("file %q does not exist", *filename)
+	} else if err != nil {
+		log.Fatalf("filesystem error checking %q: %v", *filename, err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/health" {
+			return
+		}
+
+		http.ServeFile(w, r, *filename)
+	})
+
+	log.Fatal(http.ListenAndServe(":9090", nil))
+}
